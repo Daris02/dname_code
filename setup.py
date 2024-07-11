d78e6a6b816a41acadd4df8b61c6700343766e2a
Daris02 <hei.raymond.2@gmail.com>
2024-07-11 23:07:13 +0300

    doc: update reamd file

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    description = f.read()
+
 setup(
     name='dname_code',
-    version='1.1',
+    version='1.1.2',
     packages=find_packages(),
     install_requires=[
         # Add dependencies here.
@@ -13,6 +16,8 @@ setup(
             "dname-code = dname_code:hello"
         ],
     },
+    long_description=description,
+    long_description_content_type='text/markdown',
     # author='Daris',
     # author_email='hei.raymond.2@gmail.com',
     # description='A simple Python package',
