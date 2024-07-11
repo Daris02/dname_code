49bb9577071be35cc97c720e93392d47a43fb839
Daris02 <hei.raymond.2@gmail.com>
2024-07-11 22:57:26 +0300

    feat: add entry point

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='dname_code',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         # Add dependencies here.
         # e.g. 'numpy>=1.11.1'
     ],
+    entry_points={
+        "console_scripts": [
+            "dname-code = dname_code:hello"
+        ],
+    },
     # author='Daris',
     # author_email='hei.raymond.2@gmail.com',
     # description='A simple Python package',
